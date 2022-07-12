1c34f7837e5a518fa9344f820345aaee27e0a947
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2022-07-12 11:48:39 -0300

    home screen

---
+++ .history/delivery/lib/main_20220708030223.dart
@@ -0,0 +1,23 @@
+import 'package:delivery/home/page/home.dart';
+import 'package:flutter/material.dart';
+
+import 'browse/page/browse.dart';
+import 'browse/widget/product.dart';
+import 'login/page/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: homeScreen(),
+    );
+  }
+}
